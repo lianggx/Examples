b8d01a13a5862c6e4c82a65a9b2e99d70defe64e
lianggx <@foxmail.com>
2019-03-13 23:29:26 +0800

    SignalR Lesson 1

---
+++ SignalR/Ron.SignalRLesson1/Ron.SignalRLesson1/Services/WeChatHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ron.SignalRLesson1.Services
+{
+    public class WeChatHub : Hub
+    {
+        public void Send(MessageBody body)
+        {
+            Clients.All.SendAsync("Recv", body);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("哇，有人进来了：{0}", this.Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine("靠，有人跑路了：{0}", this.Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+
+    public class MessageBody
+    {
+        public int Type { get; set; }
+        public string UserName { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ SignalR/Ron.SignalRLesson1/Ron.SignalRLesson1/wwwroot/js/wechat.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/wechatHub")
+    .build();
+
+connection.on("Recv", function (data) {
+    var li = document.createElement("li");
+    li = $(li).text(data.userName + "：" + data.content)
+    $("#msgList").append(li);
+});
+
+connection.start()
+    .then(function () {
+        console.log("SignalR 已连接");
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+$(document).ready(function () {
+    $("#btnSend").on("click", () => {
+        var userName = $("#userName").val();
+        var content = $("#content").val();
+        console.log(userName + ":" + content);
+        connection.invoke("send", { "Type": 0, "UserName": userName, "Content": content });
+    });
+});
