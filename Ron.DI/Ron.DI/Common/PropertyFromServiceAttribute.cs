872b8203401e11a7526595b292024e7b3d3ef75d
lianggx <@foxmail.com>
2019-06-25 19:59:00 +0800

    FromService Example

---
+++ Ron.DI/Ron.DI/Common/PropertyFromServiceAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class PropertyFromServiceAttribute : Attribute, IBindingSourceMetadata
+{
+    public BindingSource BindingSource => BindingSource.Services;
+}
---
+++ Ron.DI/Ron.DI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Ron.DI.Services;
+
+namespace Ron.DI.Controllers
+{
+    [Route("home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [PropertyFromService] public IConfiguration Configuration { get; set; }
+        [PropertyFromService] public IHostingEnvironment Environment { get; set; }
+        [PropertyFromService] public CarService CarService { get; set; }
+        [PropertyFromService] public PostServices PostServices { get; set; }
+        [PropertyFromService] public TokenService TokenService { get; set; }
+        [PropertyFromService] public TopicService TopicService { get; set; }
+        [PropertyFromService] public UserService UserService { get; set; }
+
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet("index")]
+        public ActionResult<string> Index()
+        {
+            return "Hello world!";
+        }
+    }
+}
